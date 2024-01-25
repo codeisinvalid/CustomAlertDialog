f4ddff92f42f3c978a96c17cb77d166f4550646f
sooj <soojksh@gmail.com>
2024-01-25 16:34:21 +0530

    ℹ️ Implement Custom AlertDialog in Android
    📝 This commit introduces a custom alert dialog implementation in the Android app. The new dialog is designed using XML layout files and implemented in Java. It provides a more user-friendly and customized experience compared to the default AlertDialog.
    
    🔧 Changes Made:
    - Added success alert layout, warning alert layout and error alert layout (.xml) for the dialog layout.
    - Updated MainActivity.java to showcase the usage of the custom alert dialog.
    
    🚀 Features:
    - Custom layout design for different alert dialogs.
    - Additional customization options for title, message, and buttons.
    - Improved user interaction and visual appeal.
    
    🛠️ How to Test:
    1. Open MainActivity and trigger the custom alert dialog.
    2. Observe the enhanced appearance and behavior of the custom dialog.
    3. Verify that all buttons and interactions work as expected.

---
+++ app/src/main/java/com/example/customalertdialog/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.customalertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.ButtonSuccess).setOnClickListener(v -> showSuccessDialog());
+
+        findViewById(R.id.ButtonWarning).setOnClickListener(v -> showWarningDialog());
+
+
+        findViewById(R.id.ButtonError).setOnClickListener(v -> showErrorDialog());
+    }
+
+    private void showSuccessDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
+        View view = LayoutInflater.from(MainActivity.this).inflate(
+                R.layout.layout_success_dialog,
+                (ConstraintLayout)findViewById(R.id.layoutDialogContainer)
+        );
+        builder.setView(view);
+        ((TextView) view.findViewById(R.id.textTitle)).setText(getResources().getString(R.string.success_title));
+        ((TextView) view.findViewById(R.id.textMessage)).setText(getResources().getString(R.string.dummy_text));
+        ((Button) view.findViewById(R.id.actionButton)).setText(getResources().getString(R.string.okay));
+        ((ImageView) view.findViewById(R.id.imageIcon)).setImageResource(R.drawable.ic_success);
+
+        AlertDialog alertDialog = builder.create();
+        view.findViewById(R.id.actionButton).setOnClickListener(v -> alertDialog.dismiss());
+
+        if (alertDialog.getWindow()!= null) {
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        alertDialog.show();
+
+    }
+    private void showWarningDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
+
+        View view = LayoutInflater.from(MainActivity.this).inflate(
+                R.layout.layout_warning_dialog,
+                (ConstraintLayout)findViewById(R.id.layoutDialogContainer)
+        );
+
+        builder.setView(view);
+
+        ((TextView) view.findViewById(R.id.textTitle)).setText(getResources().getString(R.string.warning_title));
+        ((TextView) view.findViewById(R.id.textMessage)).setText(getResources().getString(R.string.dummy_text));
+        ((Button) view.findViewById(R.id.buttonYes)).setText(getResources().getString(R.string.yes));
+        ((Button) view.findViewById(R.id.buttonNo)).setText(getResources().getString(R.string.no));
+        ((ImageView) view.findViewById(R.id.imageIcon)).setImageResource(R.drawable.ic_warning);
+
+        AlertDialog alertDialog = builder.create();
+
+        view.findViewById(R.id.buttonYes).setOnClickListener(v -> alertDialog.dismiss());
+
+        view.findViewById(R.id.buttonNo).setOnClickListener(v -> alertDialog.dismiss());
+
+        if (alertDialog.getWindow()!= null) {
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+
+        alertDialog.show();
+
+    }
+    private void showErrorDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
+        View view = LayoutInflater.from(MainActivity.this).inflate(
+                R.layout.layout_error_dialog,
+                (ConstraintLayout)findViewById(R.id.layoutDialogContainer)
+        );
+        builder.setView(view);
+        ((TextView) view.findViewById(R.id.textTitle)).setText(getResources().getString(R.string.error_title));
+        ((TextView) view.findViewById(R.id.textMessage)).setText(getResources().getString(R.string.dummy_text));
+        ((Button) view.findViewById(R.id.actionButton)).setText(getResources().getString(R.string.error));
+        ((ImageView) view.findViewById(R.id.imageIcon)).setImageResource(R.drawable.ic_error);
+
+        AlertDialog alertDialog = builder.create();
+        view.findViewById(R.id.actionButton).setOnClickListener(v -> alertDialog.dismiss());
+
+        if (alertDialog.getWindow()!= null) {
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        alertDialog.show();
+    }
+}
